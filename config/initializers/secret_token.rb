dbb545679f355ef950d32f142d6e784d94ff9af5
pedroargento <peter.argent@gmail.com>
2013-08-21 21:06:21 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotdog::Application.config.secret_token = 'b244e10ff5581ad43bea90e85aa150302858a79a0c8bbf8cb7263a067ac30fa9f8828c3467df0622744f42fe250c28f354a73a503c1331b000578cbeb8c14b1a'
